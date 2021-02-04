e26fb369e7adef77e5479d648e6c17ccd8805329
zalutin-a <an9rey89@yandex.ru>
2021-02-04 23:03:38 +0300

    add count-cats func

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,9 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats( matrix ) {
+  matrix = matrix.flat(Infinity)
+  let str = matrix.join(',')
+  let str2 = str.match(/\s\^\^(?!\ )/g)
+  str = str.match(/\^\^(?!\ )/g)
+  return str === null ? 0 : str2 === null? str.length : str.length - str2.length
 };

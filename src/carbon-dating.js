ce216efc2f1b85db61d9e563be8d1323dae9f729
zalutin-a <an9rey89@yandex.ru>
2021-02-08 18:45:57 +0300

    add func: count cats, carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,8 +2,17 @@ const CustomError = require("../extensions/custom-error");
 
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+const KOEF = Math.LN2/HALF_LIFE_PERIOD
+module.exports = function dateSample( sampleActivity ) {
+      
+      if(!arguments ||arguments.length === 0) return false
+      if(typeof(sampleActivity) !== String) return false
+      sampleActivity = parseInt(sampleActivity)
+      
+      if(sampleActivity>15||sampleActivity<=0)return false
+ 
+      let num = Math.log(MODERN_ACTIVITY/sampleActivity)
+      return Math.ceil(num/KOEF)
+      
+    
 };

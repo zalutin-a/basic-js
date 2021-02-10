2312d905eab9e0a9e11caddbf2afbbcdb2b61d21
zalutin-a <an9rey89@yandex.ru>
2021-02-10 14:17:31 +0300

    add func. transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  let arr1 = Array.from(arr)
+  if(typeof(arr)!== "object") throw 'THROWN'
+  if(!Array.isArray(arr)) throw 'THROWN'
+  if(arguments.length === 0) throw 'THROWN'
+  if(arr.length == 0) return arr
+  
+  
+  
+  for(let i = 0; i < arr1.length; i++){
+    if(arr1[i] == '--discard-next' && arr1[i+2] == '--double-prev') arr1.splice(i, 3)
+    if(arr1[i] == '--discard-next' && arr1[i+2] == '--discard-prev') arr1.splice(i, 3)
+    
+    if(arr1[i] == '--discard-prev' && i===0) arr1.splice(i, 1)
+    if(arr1[i] == '--double-prev' && i===0) arr1.splice(i, 1)
+    if(arr1[i] == '--double-next' && i===arr1.length-1) arr1.splice(i, 1)
+    if(arr1[i] == '--discard-next' && i===arr1.length-1) arr1.splice(i, 1)
+    
+    if(arr1[i] == '--double-next') arr1[i] = arr1[i+1]
+    if(arr1[i] == '--double-prev') arr1[i] = arr1[i-1]
+    if(arr1[i] == '--discard-next') {arr1.splice(i, 2); i = i - 1}
+    if(arr1[i] == '--discard-prev') {arr1.splice(i-1, 2); i = i - 2}
+  }
+  return arr1
+
 };
